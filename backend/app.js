843b565bcd73434911eab8dbc0412d100d18069e
saksham3333 <141694707+saksham3333@users.noreply.github.com>
2025-05-21 17:11:21 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ mongoose
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://medi-connect-xxsa.onrender.com"],
+    origin: ["http://localhost:3000", "medi-connect-6ift.vercel.app"],
     credentials: true,
   })
 );
